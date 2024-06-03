ff268048f3efe17917a8379b70674664ff54d33c
Eric Mika <eric@ericmika.com>
2024-06-03 19:00:13 -0400

    Add generated manifest to repository root, and don't say Obsidian in the plugin description.

--- scripts/generate-manifest.ts
+++ scripts/generate-manifest.ts
@@ -16,7 +16,7 @@ type ObsidianManifest = {
 const manifest: ObsidianManifest = {
 	author: packageJson.author.name,
 	authorUrl: packageJson.author.url,
-	description: packageJson.description,
+	description: packageJson.description.replace(/\s+obsidian\s+/i, ' '),
 	helpUrl: packageJson.homepage,
 	id: packageJson.name,
 	version: packageJson.version,
@@ -25,5 +25,9 @@ const manifest: ObsidianManifest = {
 
 await fs.mkdir('./dist', { recursive: true })
 await fs.writeFile('./dist/manifest.json', JSON.stringify(manifest, undefined, 2))
+
+// Obsidian's plugin review process also seems to require the manifest.json to
+// be in the root of the repository, even though it's in the release...
+await fs.writeFile('./manifest.json', JSON.stringify(manifest, undefined, 2))
 
 console.log(`Generated Obsidian manifest.json file.`)
