09fdfa83ac2a67308c04a8c7c4acfc0537940c4d
Eric Mika <eric@ericmika.com>
2025-03-02 04:53:58 -0500

    Update dependencies.

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,8 +1,13 @@
-import builtins from 'builtin-modules'
 import esbuild from 'esbuild'
 import { type Plugin } from 'esbuild'
 import { copy } from 'esbuild-plugin-copy'
 import process from 'node:process'
+
+// We assume our minimum specified Obsidian version 1.5.0 correlates with the
+// following:
+// - The closest release seems to be 1.5.3:
+//   https://github.com/obsidianmd/obsidian-releases/releases/tag/v1.5.3
+// This release is using Electron 25.8.1, Chromium 114, V8 11.4, and Node 18.15.0
 
 const banner = `/*
 This is a generated source file!
@@ -42,7 +47,61 @@ const context = await esbuild.context({
 		'@lezer/common',
 		'@lezer/highlight',
 		'@lezer/lr',
-		...builtins,
+
+		// Node 18 builtins
+		// https://github.com/uncenter/builtin-modules-static/blob/main/lib/v18.js
+		'assert',
+		'assert/strict',
+		'async_hooks',
+		'buffer',
+		'child_process',
+		'cluster',
+		'console',
+		'constants',
+		'crypto',
+		'dgram',
+		'diagnostics_channel',
+		'dns',
+		'dns/promises',
+		'domain',
+		'events',
+		'fs',
+		'fs/promises',
+		'http',
+		'http2',
+		'https',
+		'inspector',
+		'module',
+		'net',
+		'os',
+		'path',
+		'path/posix',
+		'path/win32',
+		'perf_hooks',
+		'process',
+		'punycode',
+		'querystring',
+		'readline',
+		'readline/promises',
+		'repl',
+		'stream',
+		'stream/consumers',
+		'stream/promises',
+		'stream/web',
+		'string_decoder',
+		'timers',
+		'timers/promises',
+		'tls',
+		'trace_events',
+		'tty',
+		'url',
+		'util',
+		'util/types',
+		'v8',
+		'vm',
+		'wasi',
+		'worker_threads',
+		'zlib',
 	],
 	format: 'cjs',
 	logLevel: 'error',
