af4e52053554357abc46102638f72a5dbce11738
Eric Mika <eric@ericmika.com>
2025-06-10 01:36:56 -0400

    Update dependencies.

--- cspell.config.js
+++ cspell.config.js
@@ -2,20 +2,14 @@ import { cspellConfig } from '@kitschpatrol/cspell-config'
 
 export default cspellConfig({
 	words: [
-		'Ankiconnect',
 		'clozed',
 		'clozing',
-		'emisjerry',
 		'encloze',
-		'Flathub',
-		'furigana',
-		'hotreload',
 		'jīng',
 		'mzietzke',
 		'noteless',
 		'outbase',
 		'subfolders',
-		'Templater',
 		'Zoé',
 	],
 })
